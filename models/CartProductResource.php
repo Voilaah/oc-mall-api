04a7f4f9f3773b140f1f3d53605f8e091a9c7f13
chrisvidal <chris@voilaah.com>
2020-07-01 17:08:14 +0800

    adding prices to cart api

--- models/CartProductResource.php
+++ models/CartProductResource.php
@@ -3,6 +3,7 @@
 namespace Voilaah\MallApi\Models;
 
 use OFFLINE\Mall\Models\Currency;
+use OFFLINE\Mall\Classes\Utils\Money;
 use Voilaah\MallApi\Models\ImageResource;
 
 Class CartProductResource extends SimpleApiResource
@@ -26,7 +27,15 @@ Class CartProductResource extends SimpleApiResource
             "url"                   => route('products.show', ['recordId' =>  $this->product->slug . '/' . $this->variant->hashId] ),
             'quantity'              => (int)$this->quantity,
             'weight'                => (int)$this->weight,
-            'price'                 => $this->price,
+            // 'item_data'             => $this->item_data,
+            'unit_price'            => $this->price,
+            'unit_price_raw'        => $this->price()->integer,
+            'unit_price_formatted'  => (string)$this->price(),
+            // 'total_pre_taxes'       => $this->total_pre_taxes,
+            // 'total_taxes'           => $this->total_taxes,
+            'total_post_taxes_raw'      => $this->total_post_taxes,
+            'total_post_taxes_formatted'=> app(Money::class)->format($this->total_post_taxes),
+            // 'price_total'           => $this->price(),
             'image'                 => ImageResource::make($this->variant->image),
             'created_at'            => (int)$this->created_at->timestamp,
             'updated_at'            => (int)$this->updated_at->timestamp,
